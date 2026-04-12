d00d987bd634b3a19075bb7121e3d72f4511056b
Marian Meres <marian@meres.sk>
2026-04-12 20:43:33 +0200

    build script

--- scripts/build-npm.ts
+++ scripts/build-npm.ts
@@ -6,4 +6,11 @@ await npmBuild({
 	name: denoJson.name,
 	version: denoJson.version,
 	repository: denoJson.name.replace(/^@/, ""),
+	dependencies: [
+		"@marianmeres/clog@^3",
+		"@marianmeres/collection-types@^1",
+		"@marianmeres/http-utils@^2",
+		"@marianmeres/pubsub@^2",
+		"@marianmeres/store@^2",
+	],
 });
